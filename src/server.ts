a80cc20a841868f0a6f3f6d75dc73e8064b7c123
jordana.reichel <jordana.reichel@quanticcompany.com>
2022-11-16 11:11:49 -0300

    [fix] tentativa de subir pra produção

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,17 @@
+// Require the framework
 import Fastify from "fastify";
-import cors from '@fastify/cors';
-import { poolRoute } from "./routes/pool";
-import { userRoute } from "./routes/user";
-import { guessRoute } from "./routes/guess";
-import { authRoute } from "./routes/auth";
-import { gameRoute } from "./routes/game";
-import jwt from "@fastify/jwt";
 
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: false,
+});
 
-async function start(){
-  const fastify = Fastify({
-    logger: true,
+// Register your application as a normal plugin.
+app.register(import("./functions/server"), {
+    prefix: '/'
+});
 
-  });
-
-  await fastify.register(cors, {
-    origin: true,
-  });
-  
-  await fastify.register(jwt, {
-    secret: 'nlwcopa',
-  });
-
-  await fastify.register(authRoute);
-  await fastify.register(poolRoute);
-  await fastify.register(userRoute);
-  await fastify.register(guessRoute);
-  await fastify.register(gameRoute);
-
-  await fastify.listen({port: 3333, host: '0.0.0.0'});
+export default async (req: any, res: any) => {
+    await app.ready();
+    app.server.emit('request', req, res);
 }
-
-start();
